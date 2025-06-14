08093a0b4b20dcc4b20b073c909249afdde3cf42
Cloud User <cloud@2021wc86684@bits-pilani.ac.in>
2025-06-14 15:30:01 +0530

    Initial commit for Jenkins SonarQube Lab

---
+++ HelloJenkins/src/main/java/com/example/hello/App.java
@@ -0,0 +1,10 @@
+package com.example.hello;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins and SonarQube!");
+    }
+}
